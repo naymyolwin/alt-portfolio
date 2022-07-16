740248a07f73cb28bbf2662aacb61867e671f4b7
Nay Myo Lwin <naymyolwin@gmail.com>
2022-07-17 04:26:03 +0700

    completed except nav close on click

---
+++ script.js
@@ -0,0 +1,15 @@
+// const navLinks = document.querySelectorAll(".nav-item");
+// const menuToggle = document.getElementById("navbarSupportedContent");
+// const bsCollapse = new bootstrap.Collapse(menuToggle);
+// navLinks.forEach((l) => {
+//   l.addEventListener("click", () => {
+//     bsCollapse.toggle();
+//   });
+// });
+
+//const menuToggle = document.getElementById("navbarNav");
+
+// document.addEventListener("click", () => {
+//   console.log("test");
+//   menuToggle.classList(".collapse.hide");
+// });
